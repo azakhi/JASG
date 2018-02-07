ad5dc8dcc468436d0e119dcfae1b63f63bbcce5f
azakhi <azak.halilibrahim@gmail.com>
2018-02-07 21:35:41 +0300

    Add flight testing scene

---
+++ Assets/Scripts/BasicFlight.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BasicFlight : MonoBehaviour {
+    public Crosshair crosshair;
+    public Text debug;
+
+    private Vector3 velocity = new Vector3(0, 0, 0);
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var spaceKey = Input.GetKey("space");
+
+        if(spaceKey)
+        {
+            velocity += transform.forward * 1 * Time.deltaTime;
+        }
+
+        transform.Rotate(0, 0, -crosshair.PosX * Time.deltaTime);
+
+        Vector3 itd = transform.TransformDirection(new Vector3(0, 0.01f, 0)) * crosshair.PosY * Time.deltaTime;
+        velocity += itd;
+
+        transform.Translate(velocity, Space.World);
+
+        Quaternion rotation = Quaternion.LookRotation(velocity, transform.up);
+        transform.rotation = rotation;
+        
+        //debug.text = "rot: " + transform.rotation.z;
+    }
+}
---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Crosshair : MonoBehaviour {
+    public float PosX = 0;
+    public float PosY = 0;
+    public Text debug;
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var mx = Input.GetAxis("Mouse X") * Time.deltaTime * 100.0f + PosX;
+        var my = Input.GetAxis("Mouse Y") * Time.deltaTime * 100.0f + PosY;
+        
+        float dist = (mx * mx + my * my);
+        if (dist > 2500)
+        {
+            dist = 50 / Mathf.Sqrt(dist);
+            mx *= dist;
+            my *= dist;
+        }
+
+        transform.Translate(mx - PosX, my - PosY, 0);
+        PosX = mx;
+        PosY = my;
+
+        //debug.text = "x: " + PosX + " y: " + PosY;
+    }
+}
